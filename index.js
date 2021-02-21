ec2c428d5c9c61869ec533cc77e3894ad94a373e
DavidG <datog309@gmail.com>
2021-02-21 16:21:19 +0400

    deploy

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const fs = require('fs');
 const app = require('./app');
 const config = require('./utils/config');
-
-const opt = {
-    key: fs.readFileSync(`${__dirname}/certs/localhost.key`),
-    cert: fs.readFileSync(`${__dirname}/certs/localhost.crt`),
-};
 
 
 const { PORT } = config;
