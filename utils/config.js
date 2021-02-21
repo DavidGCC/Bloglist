9282b502812e7a77e2106f4380d34a309c90eec3
DavidG <datog309@gmail.com>
2021-02-21 15:56:15 +0400

    connected to db

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const { PORT } = process.env;
-let { MONGODB_URL } = process.env;
+let { LISTABLOG_MONGODB: MONGODB_URL } = process.env;
 
 if (process.env.NODE_ENV === 'test') {
     MONGODB_URL = process.env.TEST_MONGODB_URL;
